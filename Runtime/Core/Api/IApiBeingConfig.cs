4d2e749956ec27653970687c2c4c062d5df1e828
Jakub Serafin <jakub.serafin@virbe.ai>
2024-07-22 15:58:33 +0200

    feature: add config v3 data model, change apiConfig references to interface ref

---
+++ Runtime/Core/Api/IApiBeingConfig.cs
@@ -0,0 +1,23 @@
+﻿using Plugins.Virbe.Core.Api;
+
+namespace Virbe.Core
+{
+    public interface IApiBeingConfig
+    {
+        string HostDomain { get; }
+        string RoomApiAccessKey { get; }
+        string RoomUrl { get; }
+        bool RoomEnabled { get; }
+        bool HasRoom { get; }
+
+        //tts
+        int AudioChannels { get; }
+        int AudioFrequency { get; }
+        int AudioSampleBits { get; }
+
+        bool HasValidApiAccessKey();
+        bool HasValidHostDomain();
+
+        RoomApiService CreateRoom(string endUserId);
+    }
+}
