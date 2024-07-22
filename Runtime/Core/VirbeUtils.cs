90b8103a392f67d8af94e987e4ed8386002beed1
Jakub Serafin <jakub.serafin@virbe.ai>
2024-07-22 16:54:07 +0200

    feature: explicit schema

--- Runtime/Core/VirbeUtils.cs
+++ Runtime/Core/VirbeUtils.cs
@@ -1,4 +1,6 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 
 
 namespace Virbe.Core
@@ -7,8 +9,29 @@ namespace Virbe.Core
     {
         public static IApiBeingConfig ParseConfig(string configJson)
         {
-            var being = JsonConvert.DeserializeObject<ApiBeingConfig>(configJson);
-            return being;
+            var jsonObject = JObject.Parse(configJson);
+            if (jsonObject.TryGetValue("schema", out JToken schemaToken))
+            {
+                if (!Enum.TryParse<SchemaVersion>(schemaToken.ToString(), true, out var result))
+                {
+                    throw new NotImplementedException($"[VIRBE] Schema version {result} is not implemented");
+                }
+                if (result == SchemaVersion.v3)
+                {
+                    return JsonConvert.DeserializeObject<ApiBeingConfigv3>(configJson);
+                }
+            }
+            else
+            {
+                return JsonConvert.DeserializeObject<ApiBeingConfig>(configJson);
+            }
+            throw new Exception("[VIRBE] Could not parse json to config");
+        }
+
+        private enum SchemaVersion
+        {
+            v2,
+            v3
         }
     }
 }
