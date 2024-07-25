f1b2514eed87482f9fb65531c6139ffe7dc0916e
Jakub Serafin <jakub.serafin@virbe.ai>
2024-07-25 17:01:05 +0200

    feature: refactor engine handlers

--- Runtime/Core/Logger/VirbeEngineLogger.cs
+++ Runtime/Core/Logger/VirbeEngineLogger.cs
@@ -1,3 +1,4 @@
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 // ReSharper disable UnusedMember.Global
@@ -19,12 +20,39 @@ namespace Virbe.Core.Logger
 
         internal void Log(string format, params object[] args)
         {
-            Logger.LogFormat(LogType.Log, $"{Tag}/{_prefix}: " + format, args);
+            if(Logger == Debug.unityLogger)
+            {
+                LoOnMainThread(LogType.Log, format, args).Forget();
+            }
+            else
+            {
+                Logger.LogFormat(LogType.Log, $"{Tag}/{_prefix}: " + format, args);
+            }
         }
 
         internal void LogError(string format, params object[] args)
         {
-            Logger.LogFormat(LogType.Error, $"{Tag}/{_prefix}: " + format, args);
+            if (Logger == Debug.unityLogger)
+            {
+                LoOnMainThread(LogType.Error, format, args).Forget();
+            }
+            else
+            {
+                Logger.LogFormat(LogType.Error, $"{Tag}/{_prefix}: " + format, args);
+            }
+        }
+
+        private async UniTaskVoid LoOnMainThread(LogType logType, string format, params object[] args)
+        {
+            await UniTask.SwitchToMainThread();
+            if (logType == LogType.Error)
+            {
+                LogError(format, args);
+            }
+            else
+            {
+                Log(format, args);
+            }
         }
     }
 }
