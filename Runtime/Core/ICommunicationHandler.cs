0482c864845da56bfeb9f92faaaf3db5c7d2ba07
Jakub Serafin <jakub.serafin@virbe.ai>
2024-07-26 14:29:19 +0200

    feature: divide virbeing in to smaller classes

---
+++ Runtime/Core/ICommunicationHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Virbe.Core.Actions;
+
+namespace Virbe.Core
+{
+    internal interface ICommunicationHandler: IDisposable
+    {
+        event Action<UserAction> UserActionFired;
+        event Action<BeingAction> BeingActionFired;
+
+        bool Initialized { get; }
+        bool AudioStreamingEnabled { get; }
+
+        Task Prepare(string userId = null, string conversationId = null);
+        Task SendSpeech(byte[] speech);
+        Task SendNamedAction(string name, string value = null);
+        Task SendText(string text);
+    }
+}
