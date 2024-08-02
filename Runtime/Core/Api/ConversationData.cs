2fbaa9c12bf0ea0507a3621c0dcd90e29284b299
Jakub Serafin <jakub.seraphin@gmail.com>
2024-08-02 18:02:53 +0200

    feature: endles socket connection WIP

--- Runtime/Core/Api/ConversationData.cs
+++ Runtime/Core/Api/ConversationData.cs
@@ -7,12 +7,14 @@ namespace Virbe.Core
         public List<SupportedPayload> SupportedPayloads { get; }
         public ConnectionProtocol ConnectionProtocol { get; }
         internal string ApiAccessKey { get; }
+        internal string Path { get; }
 
-        public ConversationData(string apiAccessKey, List<SupportedPayload> supportedPayloads, ConnectionProtocol connectionProtocol)
+        public ConversationData(string apiAccessKey, List<SupportedPayload> supportedPayloads, ConnectionProtocol connectionProtocol, string path)
         {
             SupportedPayloads = supportedPayloads;
             ApiAccessKey = apiAccessKey;
             ConnectionProtocol = connectionProtocol;
+            Path = path;
         }
     }
 }
