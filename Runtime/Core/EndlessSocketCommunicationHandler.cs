da4231124ea8b17e33e964fb71c2f0acdf0ded0f
Jakub Serafin <jakub.seraphin@gmail.com>
2024-08-05 14:07:47 +0200

    feature: working endless ws connection

--- Runtime/Core/EndlessSocketCommunicationHandler.cs
+++ Runtime/Core/EndlessSocketCommunicationHandler.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -15,6 +17,14 @@ namespace Virbe.Core
 {
     internal sealed class EndlessSocketCommunicationHandler : ICommunicationHandler
     {
+        private const string InitializationRequestName = "conversation-init";
+        private const string SpeechRecognized = "speech-recognized";
+        private const string SpeechInitialize = "speech-start";
+        private const string SpeechEnd = "speech-end";
+        private const string SpeechAudio = "speech-audio";
+        private const string ConversationError = "conversation-error";
+        private const string StateChanged = "state";
+
         bool ICommunicationHandler.Initialized => _initialized;
         private readonly RequestActionType _definedActions = RequestActionType.SendAudioStream;
 
@@ -71,7 +81,7 @@ namespace Virbe.Core
             {
                 if (_socketClient.Connected && _speechBytesAwaitingSend.TryDequeue(out var chunk))
                 {
-                    await _socketClient.EmitAsync("conversation-speech", AudioConverter.FromBytesToBase64(chunk));
+                    await _socketClient.EmitAsync(SpeechAudio, AudioConverter.FromBytesToBase64(chunk));
                 }
                 if (cancelationToken.IsCancellationRequested)
                 {
@@ -104,7 +114,7 @@ namespace Virbe.Core
 
             _currentSttResult.Clear();
 
-            _socketClient.On("recognizing", (response) =>
+            _socketClient.On(SpeechRecognized, (response) =>
             {
               //  JArray jsonArray = JArray.Parse(response.ToString());
                // string result = (string)jsonArray[0]["text"];
@@ -112,19 +122,20 @@ namespace Virbe.Core
                 //_currentSttResult.Append(result);
             });
 
-            _socketClient.On("state", (response) =>
+            _socketClient.On(StateChanged, (response) =>
             {
                 _logger.Log($"[{DateTime.Now}]Conversation state changed : {response}");
+                var responseMessage = response.ToString();
+                var state = JsonConvert.DeserializeObject<List<StateMessage>>(responseMessage);
+                if(state.FirstOrDefault()?.IndicateInitialization == true)
+                {
+                    OnInitialized().Forget();
+                }
             });
 
             _socketClient.OnConnected += (sender, args) =>
             {
                 OnConnected().Forget();
-            };
-
-            _socketClient.OnError += (sender, args) =>
-            {
-                _logger.Log($"Socket error: {args}");
             };
 
             _socketClient.OnDisconnected += async (sender, args) =>
@@ -134,6 +145,16 @@ namespace Virbe.Core
                 _logger.Log($"Disconnected from the stt socket {args}");
             };
 
+            _socketClient.OnError += (sender, args) =>
+            {
+                _logger.Log($"Socket error: {args}");
+            };
+            
+            _socketClient.On(ConversationError, (response) =>
+            {
+                _logger.Log($"[{DateTime.Now}]Conversation error occured : {response}");
+            });
+
             _logger.Log($"Try connecting to socket.io endpoint: {_baseUrl}{_data.Path}");
             return _socketClient.ConnectAsync(_endlessSocketTokenSource.Token);
         }
@@ -142,26 +163,40 @@ namespace Virbe.Core
         {
             _logger.Log($"Connected to the socket .");
             await InitializeConversation();
+        }
+
+        private async UniTaskVoid OnInitialized()
+        {
+            _logger.Log($"Conversation initialized - starting speech");
+
+            await InitializeSpeech();
 
             _audioSocketSenderTokenSource?.Cancel();
             _audioSocketSenderTokenSource = new CancellationTokenSource();
             SocketAudioSendLoop(_audioSocketSenderTokenSource.Token).Forget();
         }
-
         private async Task InitializeConversation()
         {
-            var msg = new InitializeMessage() { endUserId = _currentSession.UserId, conversationId = _currentSession.ConversationId };
-            var json = JsonConvert.SerializeObject(msg);
-            _logger.Log(json);
-            //TODO: fix [WebSocket⬆] 42["conversation-init","{\"endUserId\":\"21a7c0a2-9b4c-4934-ad72-e203b7814930\",\"conversationId\":null}"]
-            //[WebSocket⬇] 42["conversation-error",{ "code":"handler-wrong-state","current":"socket","expected":"initialized"}]
-            await _socketClient.EmitAsync("conversation-init", json);
+            var conversationId = _currentSession.ConversationId ?? Guid.Empty.ToString();
+            if (string.IsNullOrEmpty(_currentSession.UserId))
+            {
+                _logger.LogError($"{nameof(_currentSession.UserId)} must be UUID.");
+                return;
+            }
+            var msg = new InitializeMessage() { endUserId = _currentSession.UserId };
+            if (!string.IsNullOrEmpty(_currentSession.ConversationId))
+            {
+                msg.conversationId = _currentSession.ConversationId;
+            }
+            await _socketClient.EmitAsync(InitializationRequestName, msg);
+           //_logger.Log(msg.ToString());
         }
 
-        private async UniTaskVoid SendMessage()
+        private async Task InitializeSpeech()
         {
-            //conversation-message
-            //await _socketClient.EmitAsync("conversation-message", JsonConvert.SerializeObject(msg));
+            var msg = new SpeachInitMessage() {enableContinuousRecognition = true.ToString(), sendResultsToConversationEngine = true.ToString() };
+            await _socketClient.EmitAsync(SpeechInitialize, msg);
+            _logger.Log(msg.ToString());
         }
 
         private void SendTextFromRresult()
@@ -204,6 +239,31 @@ namespace Virbe.Core
         {
             public string endUserId { get; set; }
             public string conversationId { get; set; }
+
+            public override string ToString()
+            {
+                return $"endUserId = {endUserId}, conversationId = {conversationId}";
+            }
+        }
+
+        private class SpeachInitMessage
+        {
+            public string enableContinuousRecognition { get; set; }
+            public string sendResultsToConversationEngine { get; set; }
+
+            public override string ToString()
+            {
+                return $"enableContinuousRecognition = {enableContinuousRecognition}, sendResultsToConversationEngine = {sendResultsToConversationEngine}";
+            }
+        }
+        // [{"previous":"socket","current":"initialized"}]
+
+        private class StateMessage
+        {
+            public string previous { get; set; }
+            public string current { get; set; }
+
+            public bool IndicateInitialization => previous == "socket" && current == "initialized";
         }
     }
 }
