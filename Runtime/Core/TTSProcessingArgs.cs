5b43398432aaf14ad5e006da3570d2dc3f655848
Jakub Serafin <jakub.seraphin@gmail.com>
2024-08-07 14:35:18 +0200

    feature: add onDemand speech sending - it's fast, add proper queue for tts response

---
+++ Runtime/Core/TTSProcessingArgs.cs
@@ -0,0 +1,19 @@
+﻿using Plugins.Virbe.Core.Api;
+using System;
+
+namespace Virbe.Core
+{
+    public struct TTSProcessingArgs
+    {
+        public readonly string Text;
+        public readonly Guid ID;
+        public readonly Action<RoomDto.BeingVoiceData> Callback;
+
+        public TTSProcessingArgs (string text, Guid id,Action<RoomDto.BeingVoiceData> callback)
+        {
+            Text = text;
+            Callback = callback;
+            ID = id;
+        }
+    }
+}
