069fd720ebf83f23c775775e9ab6520a9af3b6f9
Jakub Serafin <jakub.seraphin@gmail.com>
2024-08-08 14:15:15 +0200

    feature: add base vad class

---
+++ Runtime/Core/VAD/VADActivateTarget.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+namespace Virbe.Core.VAD
+{
+    public class VADActivateTarget : MonoBehaviour
+    {
+        private Collider _targetCollider;
+        private const float _maxWidthSize = 1f;
+        private void Awake()
+        {
+            _targetCollider = GetComponent<Collider>();
+            if(_targetCollider == null ) 
+            {
+                var collider = gameObject.AddComponent<BoxCollider> ();
+                collider.isTrigger = true;
+                var bounds = new Bounds();
+                foreach(var renderer in GetComponentsInChildren<Renderer>()) 
+                {
+                    bounds.Encapsulate(renderer.bounds);
+                }
+                collider.center = bounds.center;
+                bounds.size = new Vector3(Mathf.Clamp(bounds.size.x, 0, _maxWidthSize), bounds.size.y, bounds.size.z) ;
+                collider.size = bounds.size;
+                _targetCollider = collider;
+            }
+        }
+
+        public void SetMask(string maskName)
+        {
+            gameObject.layer = LayerMask.NameToLayer(maskName);
+        }
+    }
+}
